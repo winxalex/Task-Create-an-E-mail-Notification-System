e2e9fb3dc6696690151a100eadf13787aa62b3b8
alexwinx <winxalex@gmail.com>
2024-08-25 20:55:30 +0200

    UPGRADE: add some authorization modules CLEAN

--- AlexMedia/AlexMediaSolution/Frontend/Program.cs
+++ AlexMedia/AlexMediaSolution/Frontend/Program.cs
@@ -97,60 +97,10 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
-
-//using Frontend.Components;
-//using Microsoft.AspNetCore.Components;
-//using Microsoft.AspNetCore.Components.Web;
-//using Microsoft.AspNetCore.Components.Authorization;
-//using Microsoft.Identity.Web;
-//using Microsoft.Identity.Web.UI;
-////using AlexMedia.EmailNotificationSystem.Services;
-////using AlexMedia.EmailNotificationSystem.Data;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddRazorPages();
-//builder.Services.AddServerSideBlazor();
-////builder.Services.AddSingleton<ClientDataService>();
-////builder.Services.AddSingleton<TemplateService>();
-////builder.Services.AddSingleton<CampaignService>();
-
-//// Add Azure AD Authentication
-//builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "AzureAd")
-//    .EnableTokenAcquisitionToCallDownstreamApi()
-//    .AddInMemoryTokenCaches();
-
-//builder.Services.AddControllersWithViews()
-//    .AddMicrosoftIdentityUI();
-
-//builder.Services.AddAuthorization(options =>
-//{
-//    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
-//});
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error");
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-//app.UseRouting();
-//app.UseAuthentication();
-//app.UseAuthorization();
-
-//app.MapControllers();
-//app.MapBlazorHub();
-////app.MapFallbackToPage("/_Host");
-
-//app.Run();
